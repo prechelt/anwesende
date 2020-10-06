9bbe56a507460412cc087a2bb416148f9b50760e
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2020-10-06 16:25:36 +0200

    after cookiecutter gh:pydanny/cookiecutter-django

---
+++ anwesende/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from anwesende.users.models import User
+from anwesende.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
