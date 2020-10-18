29d6116210d934be038e8c5b5f99a5cfdd8af3af
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2020-10-18 18:45:14 +0200

    test_get_overlapping_visits

--- anwesende/room/tests/test_models.py
+++ anwesende/room/tests/test_models.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import random
 import typing as tg
 
@@ -6,6 +5,7 @@ import django.utils.timezone as djut
 import pytest
 
 import anwesende.room.models as arm
+import anwesende.utils.date as aud
 
 
 @pytest.mark.django_db
@@ -23,21 +23,15 @@ def test_get_overlapping_visits():
             results.append(seat)
         return tuple(results)
     
-    def _make_visit(seat: arm.Seat, tfrom: tg.Tuple, tto: tg.Tuple) -> arm.Visit:
-        def _thetime(hhmm: tg.Tuple[int]):
-            timehhmm = dt.time(hhmm[0], hhmm[1], 
-                               tzinfo=djut.get_current_timezone())
-            result = dt.datetime.combine(date=reftime, time=timehhmm,
-                                         tzinfo=djut.get_current_timezone())
-            print("timehhmm", timehhmm, " thetime", result)
-            return result
-        
-        present_from = _thetime(tfrom)
-        present_to = _thetime(tto)
+    def _make_visit(seat: arm.Seat, tfrom: str, tto: str) -> arm.Visit:
+        now = djut.now()
+        present_from = aud.make_dt(now, tfrom)
+        present_to = aud.make_dt(now, tto)
+        assert present_from < present_to
         v = arm.Visit(givenname="gn", familyname="fn", 
                       street_and_number="sn", zipcode="12345", town="t",
                       phone="p", email="gn@fn.de",
-                      submission_dt=djut.now(), 
+                      submission_dt=now, 
                       present_from_dt=present_from, present_to_dt=present_to,
                       seat=seat)
         v.save()
@@ -45,29 +39,27 @@ def test_get_overlapping_visits():
 
     importstep = arm.Importstep(randomkey=str(random.randrange(100000, 999999)))
     importstep.save()
-    reftime = djut.now()
     r1s1, r1s2 = _make_seats(importstep, "room1", 2)
     r2s1, = _make_seats(importstep, "room2", 1)
-    targetvisit = _make_visit(r1s1, (3, 00), (4, 00))
-    shorttargetvisit = _make_visit(r1s1, (3, 00), (3, 1))
-    # the following other visits have _y if they are to be found, _n if not:
-    otherroom_n = _make_visit(r2s1, (3, 00), (4, 00))
-    before_n = _make_visit(r1s2, (2, 00), (3, 00))
-    within_y = _make_visit(r1s2, (3, 15), (3, 45))
-    across_y = _make_visit(r1s2, (2, 00), (5, 00))
-    after_n = _make_visit(r1s2, (4, 00), (5, 00))
-    halfbefore_y = _make_visit(r1s2, (2, 30), (3, 30))
-    halfafter_y = _make_visit(r1s2, (3, 30), (4, 30))
-    nearlybefore_n = _make_visit(r1s2, (2, 00), (3, 1))
-    nearlyafter_n = _make_visit(r1s2, (3, 59), (5, 00))
+    targetvisit = _make_visit(r1s1, "03:00", "04:00")
+    shorttargetvisit = _make_visit(r1s1, "03:00", "03:01")
+    #--- the following other visits have _y if they are to be found, _n if not:
+    otherroom_n = _make_visit(r2s1, "03:00", "04:00")
+    before_n = _make_visit(r1s2, "02:00", "03:00")
+    within_y = _make_visit(r1s2, "03:15", "03:45")
+    across_y = _make_visit(r1s2, "02:00", "05:00")
+    after_n = _make_visit(r1s2, "04:00", "05:00")
+    halfbefore_y = _make_visit(r1s2, "02:30", "03:30")
+    halfafter_y = _make_visit(r1s2, "03:30", "04:30")
+    nearlybefore_n = _make_visit(r1s2, "02:00", "03:01")
+    nearlyafter_n = _make_visit(r1s2, "03:59", "05:00")
     #--- now look which ones appear:
     results = set(targetvisit.get_overlapping_visits())
-    print("djut.get_current_timezone() =", djut.get_current_timezone().__repr__())
-    print("results", results)
-    expected = set((within_y, across_y, halfbefore_y, halfafter_y))
-    not_expected = set((otherroom_n, before_n, after_n, nearlybefore_n, nearlyafter_n))
+    result_pks = set(el.pk for el in results)
+    expected = set(el.pk for el in (targetvisit, within_y, across_y, halfbefore_y, halfafter_y))
+    not_expected = set(el.pk for el in (otherroom_n, before_n, after_n, nearlybefore_n, nearlyafter_n))
+    print("result_pks", result_pks)
     print("expected", expected)
     print("not_expected", not_expected)
-    assert False
-    assert results.isdisjoint(not_expected)
-    assert results == expected
+    assert result_pks.isdisjoint(not_expected)
+    assert result_pks == expected
