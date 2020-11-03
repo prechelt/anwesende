6c0ebb3860cc524b3d7ea84347b33246e8ef06f0
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2020-11-03 12:08:19 +0100

    Users.get_datenverwalter_group(): remove caching (broke test isolation)

--- anwesende/room/management/commands/make_base_data.py
+++ anwesende/room/management/commands/make_base_data.py
@@ -9,5 +9,5 @@ class Command(djcmb.BaseCommand):
     help = "Silently creates group 'datenverwalter'"
 
     def handle(self, *args, **options):
-        aum.User.get_datenverwalter_group()
+        aum.User.get_datenverwalter_group()  # so admin has it on first visit
         arm.Seat.get_dummy_seat()  # create now to make it nicely the first one
--- anwesende/users/models.py
+++ anwesende/users/models.py
@@ -7,7 +7,6 @@ from django.utils.translation import gettext_lazy as _
 class User(djcam.AbstractUser):
     """Default user for anwesende."""
     STAFF_GROUP = "datenverwalter"
-    _datenverwalter_group = None  # cache
     #: First and last name do not cover name patterns around the globe:
     name = CharField(_("Name of User"), blank=True, max_length=255)
 
@@ -15,16 +14,15 @@ class User(djcam.AbstractUser):
         return reverse("users:detail", kwargs={"username": self.username})
 
     def assign_datenverwalter_group(self) -> None:
-        self.groups.add(self.get_datenverwalter_group())
+        group = self.get_datenverwalter_group()
+        assert group.id == 1
+        self.groups.add(group)
 
     def is_datenverwalter(self) -> bool:
-        return self.groups.filter(pk=self.get_datenverwalter_group().pk).exists()
+        return self.groups.filter(id=self.get_datenverwalter_group().id).exists()
 
     @classmethod
     def get_datenverwalter_group(cls) -> djcam.Group:
-        if cls._datenverwalter_group is None:
-            if not djcam.Group.objects.filter(name=cls.STAFF_GROUP).exists():
-                djcam.Group.objects.create(name=cls.STAFF_GROUP)
-            cls._datenverwalter_group = djcam.Group.objects.get_by_natural_key(
-                cls.STAFF_GROUP)
-        return cls._datenverwalter_group
+        if not djcam.Group.objects.filter(name=cls.STAFF_GROUP).exists():
+            fresh_group = djcam.Group.objects.create(name=cls.STAFF_GROUP)
+        return djcam.Group.objects.get_by_natural_key(cls.STAFF_GROUP)
